fb7f615aaee8e53ffbbe609d0ef6cba90f31ce40
David Botton <david@botton.com>
2022-05-16 19:09:56 -0400

    update clogpower to use clog-web-sight

--- clogpower.lisp
+++ clogpower.lisp
@@ -1,11 +1,193 @@
 ;; Setup or clogpower package.
 (defpackage #:clogpower
-  (:use #:cl #:clog #:clog-gui)
+  (:use #:cl #:clog #:clog-web #:clog-gui #:clog-auth #:clog-web-dbi)
   (:export start-site))
 
 (in-package :clogpower)
 
-(defun main-screen (body)
+;;
+;; Setup website structure, database and CLOG
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar *sql-connection* nil)
+
+;; We use authorizations to control what menus appear if logged in or not.
+;; We use the :authorize key on create-web-page to limit access as well
+;; directly to a page when not using menus.
+;; We use our menu with the convenient clog-web-routes-from-menu to
+;; setup the routes from URLs to handlers:
+
+                      ; Menu         Menu Item         URL        Handler         Actions Auth
+(defparameter *menu* `(("Features" (("Login"           "/login"   on-login        :login)
+				    ("Signup"          "/signup"  on-signup       :signup)
+				    ("Change Password" "/pass"    on-new-pass     :change-password)
+				    ("Home"            "/content" on-clogpower    :content)
+				    ("Demo"            "/demo"    on-demo-window)
+				    ("Logout"          "/logout"  on-logout       :logout)))
+		       ("Admin"    (("User List"       "/users"   on-users        :users)))
+		       ("Help"     (("About"           "/about"   on-about))))
+  "Setup website menu")
+
+(defun add-search-optimizations (path content)
+  ;; The default boot.html that comes with CLOG has template
+  ;; markers inside of the meta section and body section
+  ;; that are set to be transparent to the user but show to
+  ;; search engines and text browser. This allows setting
+  ;; custom data for search engine optimizations which are
+  ;; aware of these type of dynamic sites.
+  (declare (ignore path))
+  (funcall (cl-template:compile-template content)
+	   (list :meta "<meta name='description' content='CLOGPower.com CLOG Framework Home Page'>
+                        <meta name='author' content='David Botton'>
+                        <meta name='keywords' content='Common Lisp, GUI, Web Framework, CLOG'>"
+		 :body "CLOGPower.com - CLOG the Common Lisp Omnificient GUI
+                        a site dedicated to CLOG.")))
+
+(defun start-site ()
+  ;; Setup authorizations between roles and actions
+  ;; multiple roles can be activated to multiple actions
+  ;; A user can be assign many roles
+  (add-authorization '(:guest :member) '(:content
+					 :content-show-comments))
+  (add-authorization '(:guest)  '(:login :signup))
+  (add-authorization '(:member) '(:logout
+				  :change-password
+				  :content-comment))
+  (add-authorization '(:editor) '(:content-edit))
+  (add-authorization '(:admin)  '(:users :content-admin))
+  ;; Setup database connection
+  (when *sql-connection*
+    (dbi:disconnect *sql-connection*))
+  (let ((db-dir (format nil "~A~A" (asdf:system-source-directory :cweb) "clogpower.db")))
+    (setf *sql-connection* (dbi:connect :sqlite3 :database-name db-dir))
+    (format t "Database location: ~A~%" db-dir))
+  ;; Check if need to setup sample data
+  (handler-case
+      (dbi:fetch (dbi:execute (dbi:prepare *sql-connection* "select * from config")))
+    (error ()
+      (print "Create database and tables.")
+      (create-base-tables *sql-connection*)))
+  ;; Turn off debugger launches for production runs
+  (setf clog-connection:*break-on-error* nil)
+  (initialize 'on-clogpower :boot-function 'add-search-optimizations
+			    :long-poll-first t
+			    :extended-routing t
+			    :static-root (merge-pathnames "./www/"
+							  (asdf:system-source-directory :clogpower)))
+  (clog-web-routes-from-menu *menu*)
+  ;; open-browser when testing, fails to open on production server.
+  (open-browser))
+
+;;
+;; Look and Feel
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun init-site (body)
+  "Setup the website, called on each url switch"
+  ;; Initialize the clog-web environment
+  (clog-web-initialize body)
+  ;; Instantly reload other windows open on authentication change
+  (set-on-authentication-change body (lambda (body)
+				       (url-replace (location body) "/")))
+  ;; Initialzie the clog-web-site environment
+  (let ((profile (get-profile body *sql-connection*)))
+    (create-web-site body
+		     :settings '(:color-class  "w3-amber"
+				 :border-class ""
+				 :signup-link  "/signup"
+				 :login-link   "/login")
+		     :profile profile
+		     :roles (if profile
+				(if (equalp "admin"
+					    (getf profile :|username|))
+				    '(:member :editor :admin)
+				    '(:member))
+				'(:guest))
+		     :title "CLOG - The Common Lisp Omnificent GUI"
+		     :footer "(c) 2022 David Botton"
+		     :logo "/img/clog-logo.png")))
+
+;;
+;; URL Path Handlers
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun on-clogpower (body)
+  (init-site body)
+  (create-web-page body :index `(:menu    ,*menu*
+				 :content ,(clog-web-content *sql-connection*
+							     :comment-table "content"))))
+
+(defun on-login (body)
+  (init-site body)
+  (create-web-page
+   body
+   :login `(:menu      ,*menu*
+	    :on-submit ,(lambda (obj)
+			  (if (login body *sql-connection*
+				     (name-value obj "username")
+				     (name-value obj "password"))
+			      (url-replace (location body) "/")
+			      (clog-web-alert obj "Invalid" "The username and password are invalid."
+					      :time-out 3
+					      :place-top t))))
+   :authorize t))
+
+(defun on-logout (body)
+  (logout body)
+  (url-replace (location body) "/"))
+
+(defun on-signup (body)
+  (init-site body)
+  (create-web-page body
+		   :signup `(:menu    ,*menu*
+			     :content ,(lambda (body)
+					 (sign-up body *sql-connection*)))
+		   :authorize t))
+
+(defun on-about (body)
+  (init-site body)
+  (create-web-page body :about `(:menu    ,*menu*
+				 :content "<img width=300 src='/img/clog-logo.png'>")))
+
+(defun on-users (body)
+  (init-site body)
+  (create-web-page body :users
+		   `(:menu    ,*menu*
+		     :content ,(lambda (body)
+				 (let ((users (dbi:fetch-all
+					       (dbi:execute
+						(dbi:prepare
+						 *sql-connection*
+						 "select * from users")))))
+				   (dolist (user users)
+				     (let* ((box   (create-div body))
+					    (suser (create-span box :content (getf user :|username|)))
+					    (rbut  (create-button box :content "Reset Password"
+								      :class "w3-margin-left")))
+				       (declare (ignore suser))
+				       (set-on-click rbut (lambda (obj)
+							    (declare (ignore obj))
+							    (reset-password *sql-connection*
+									    (getf user :|username|))
+							    (setf (disabledp rbut) t)
+							    (setf (text rbut) "Done"))))))))
+			:authorize t))
+
+(defun on-new-pass (body)
+  (init-site body)
+  (create-web-page body
+		   :change-password `(:menu    ,*menu*
+				      :content ,(lambda (body)
+						  (change-password body *sql-connection*)))
+		   :authorize t))
+
+;; Old Demosite
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun demo-main-screen (body)
   "The main screen of the clogpower.com website"
   ;; Load some nicer fonts from google
   (load-css (html-document body)
@@ -89,7 +271,7 @@
 		       :text (text-value (chat-area panel)))
   (setf (text-value (chat-area panel)) ""))
 
-(defun on-new-window (body)
+(defun on-demo-window (body)
   "New browser connection made, display splash screen"
   ;; Set our title in browser
   (setf (title (html-document body)) "clogpower.com")
@@ -112,7 +294,7 @@
 			 (declare (ignore obj))
 			 (setf done t)
 			 (setf (inner-html body) "")
-			 (main-screen body))
+			 (demo-main-screen body))
 		  :one-time t)
     ;; Animate blimp
     (setf (positioning blimp) :absolute)
@@ -126,25 +308,3 @@
     (unless done
       (setf (inner-html body) "")
       (main-screen body))))
-
-(defun add-search-optimizations (path content)
-  ;; The default boot.html that comes with CLOG has template
-  ;; markers inside of the meta section and body section
-  ;; that are set to be transparent to the user but show to
-  ;; search engines and text browser. This allows setting
-  ;; custom data for search engine optimizations which are
-  ;; aware of these type of dynamic sites.
-  (declare (ignore path))
-  (funcall (cl-template:compile-template content)
-	   (list :meta "<meta name='description' content='CLOGPower.com CLOG Framework Home Page'>
-                        <meta name='author' content='David Botton'>"
-		 :body "CLOGPower.com - CLOG the Common Lisp Omnificient GUI
-                        a site dedicated to CLOG.")))
-
-(defun start-site ()
-  ;; Turn off debugger launches for production runs
-  (setf clog-connection:*break-on-error* nil)
-  (initialize 'on-new-window :boot-function 'add-search-optimizations
-   :static-root (merge-pathnames "./www/"
-		  (asdf:system-source-directory :clogpower)))
-  (open-browser))
